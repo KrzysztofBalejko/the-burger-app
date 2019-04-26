ea884c8c289e69707be19d10a5be01aa7857938b
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-04-26 19:46:03 +0100

    :bug: issues with modules resolved

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import classes from './BurgerIngredient.css'
+import classes from './BurgerIngredient.module.css'
 import PropTypes from 'prop-types';
 
 class BurgerIngredient extends Component{
@@ -7,57 +7,35 @@ class BurgerIngredient extends Component{
     render(){
         
         let ingredient = null;
-        let x = this.props.type
 
-        if(x === "bread-bottom"){
-            ingredient = (<div className={classes.BreadBottom}>bread bottom</div>);
-        } else if(x === "bread-top"){
-            ingredient = (
-            <div className={classes.BreadTop}>
-                <div className={classes.Seeds1}>bread top</div>
-                <div className={classes.Seeds2}></div>
-            </div>
-            )
-        } else if(x === "meat"){
-            ingredient = <div className={classes.Meat}></div>
-        } else if(x === "cheese"){
-            ingredient = <div className={classes.Cheese}></div>
-        } else if(x === "salad"){
-            ingredient = <div className={classes.Salad}></div>
-        } else if(x === "bacon"){
-            ingredient = <div className={classes.Bacon}></div>
-        } else {
+        switch (this.props.type) {
+            case ("bread-bottom"):
+                ingredient = (<div className={classes.BreadBottom}></div>);
+                break;
+            case ("bread-top"):
+                ingredient = (
+                    <div className={classes.BreadTop}>
+                        <div className={classes.Seeds1}></div>
+                        <div className={classes.Seeds2}></div>
+                    </div>
+                )
+                break;
+            case ("meat"):
+                ingredient = <div className={classes.Meat}></div>
+                break;
+            case ("cheese"):
+                ingredient = <div className={classes.Cheese}></div>
+                break;
+            case ("salad"):
+                ingredient = <div className={classes.Salad}></div>
+                break;
+            case ("bacon"):
+                ingredient = <div className={classes.Bacon}></div>
+                break;
+            default:
             ingredient = null
-        }
-
-        // switch (this.props.type) {
-        //     case ("bread-bottom"):
-        //         ingredient = (<div className={classes.BreadBottom}></div>);
-        //         break;
-        //     case ("bread-top"):
-        //         ingredient = (
-        //             <div className={classes.BreadTop}>
-        //                 <div className={classes.Seeds1}></div>
-        //                 <div className={classes.Seeds2}></div>
-        //             </div>
-        //         )
-        //         break;
-        //     case ("meat"):
-        //         ingredient = <div className={classes.Meat}></div>
-        //         break;
-        //     case ("cheese"):
-        //         ingredient = <div className={classes.Cheese}></div>
-        //         break;
-        //     case ("salad"):
-        //         ingredient = <div className={classes.Salad}></div>
-        //         break;
-        //     case ("bacon"):
-        //         ingredient = <div className={classes.Bacon}></div>
-        //         break;
-        //     default:
-        //     ingredient = null
             
-        // }
+        }
         return ingredient;
       
     }
