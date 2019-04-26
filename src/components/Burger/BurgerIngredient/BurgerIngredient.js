52ae858bffd75d39ef55b81a814cc4c9f40c5af4
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-04-26 19:01:55 +0100

    :bug: :construction: debugging -BurgerIngredients do not render

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -3,40 +3,63 @@ import classes from './BurgerIngredient.css'
 import PropTypes from 'prop-types';
 
 class BurgerIngredient extends Component{
-    // eslint-disable-next-line default-case
-
+    
     render(){
-
+        
         let ingredient = null;
+        let x = this.props.type
 
-        switch (this.props.type) {
-            case('bread-bottom'):
-                ingredient = <div className={classes.BreadBottom}></div>;
-                break;
-            case('bread-top'):
-                ingredient = (
-                    <div className={classes.BreadTop}>
-                        <div className={classes.Seeds1}></div>
-                        <div className={classes.Seeds2}></div>
-                    </div>
-                )
-                break;
-            case('meat'):
-                ingredient = <div className={classes.Meat}></div>
-                break;
-            case('cheese'):
-                ingredient = <div className={classes.Cheese}></div>
-                break;
-            case('salad'):
-                ingredient = <div className={classes.Salad}></div>
-                break;
-            case('bacon'):
-                ingredient = <div className={classes.Bacon}></div>
-                break;
-            default:
+        if(x === "bread-bottom"){
+            ingredient = (<div className={classes.BreadBottom}>bread bottom</div>);
+        } else if(x === "bread-top"){
+            ingredient = (
+            <div className={classes.BreadTop}>
+                <div className={classes.Seeds1}>bread top</div>
+                <div className={classes.Seeds2}></div>
+            </div>
+            )
+        } else if(x === "meat"){
+            ingredient = <div className={classes.Meat}></div>
+        } else if(x === "cheese"){
+            ingredient = <div className={classes.Cheese}></div>
+        } else if(x === "salad"){
+            ingredient = <div className={classes.Salad}></div>
+        } else if(x === "bacon"){
+            ingredient = <div className={classes.Bacon}></div>
+        } else {
             ingredient = null
         }
+
+        // switch (this.props.type) {
+        //     case ("bread-bottom"):
+        //         ingredient = (<div className={classes.BreadBottom}></div>);
+        //         break;
+        //     case ("bread-top"):
+        //         ingredient = (
+        //             <div className={classes.BreadTop}>
+        //                 <div className={classes.Seeds1}></div>
+        //                 <div className={classes.Seeds2}></div>
+        //             </div>
+        //         )
+        //         break;
+        //     case ("meat"):
+        //         ingredient = <div className={classes.Meat}></div>
+        //         break;
+        //     case ("cheese"):
+        //         ingredient = <div className={classes.Cheese}></div>
+        //         break;
+        //     case ("salad"):
+        //         ingredient = <div className={classes.Salad}></div>
+        //         break;
+        //     case ("bacon"):
+        //         ingredient = <div className={classes.Bacon}></div>
+        //         break;
+        //     default:
+        //     ingredient = null
+            
+        // }
         return ingredient;
+      
     }
 
 };
