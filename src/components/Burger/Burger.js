9186f8a76ee1a94471f7cb71a7a5e710b391f918
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-04-27 13:13:23 +0100

    :building_construction: dynamic ingredients display

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,30 @@ import React from 'react';
 import classes from './Burger.module.scss'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
+
+
+
 const burger = (props) => {
+
+    const dynamicIngredients = (props) => {
+
+        let food = Object.keys(props.ingredients)
+        let amount = Object.values(props.ingredients)
+        let arr = [];
+      
+        food.forEach((key, index) => {
+            for (let i of Array(amount[index]).keys()){
+              arr.push(key)
+            };
+        })
+        return arr.map((item) => <BurgerIngredient type={item} /> )
+        
+    }
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {dynamicIngredients(props)}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
