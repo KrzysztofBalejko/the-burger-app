e258494c92c26f10b4625fb19494d06f87e0045a
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-05-03 21:54:47 +0100

    :recycle: refactoring BuildControl component

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,9 +4,18 @@ import classes from './BuildControl.module.scss'
 const buildControl = (props) => {
     return(
         <div className={classes.BuildControl}>
-            <div className={classes.Label}>{props.label}</div>
-            <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
-            <button className={classes.More} onClick={props.added}>More</button>
+            <div className={classes.Label}>
+                {props.label}
+            </div>
+            <button className={classes.Less}
+                onClick={props.removed}
+                disabled={props.disabled}>
+                Less
+            </button>
+            <button className={classes.More}
+                onClick={props.added}>
+                More
+            </button>
         </div>
     );
 
