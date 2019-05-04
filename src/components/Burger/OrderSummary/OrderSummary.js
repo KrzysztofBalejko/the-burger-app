d61101033e3880a670666b1bfed2e63609574139
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-05-04 13:24:26 +0100

    :building_construction: OrderSummary update

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import Aux from '../../../hoc/Aux'
 
 const orderSummary = (props) => {
-
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return(
+                <li>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                </li>  
+            ) 
+        })
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingrdients</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Contine to Checkout?</p>
+        </Aux>
+    )
 };
 
 export default orderSummary;
