7d68d99bcf917ff80393e0d93bc663b94166cbb0
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-05-05 21:17:06 +0100

    :building_construction: changes to new components

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import classes from './NavigationItem.module.scss'
+
 
 const navigationItem = (props) => (
     <ul>
-        <li><a href="/">A Link</a></li>
+        <li className={classes.NavigationItem}>
+        <a href={props.link}
+        className={props.active ? classes.active : null}>{props.children}</a></li>
     </ul>
 );
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import classes from './NavigationItems.module.scss'
+import NavigationItem from './NavigationItem/NavigationItem'
+
 
 const navigationItems = () => (
-    <ul>
-        <li><a href="/">A Link</a></li>
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" active={true}>Burger Builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
     </ul>
 );
 
