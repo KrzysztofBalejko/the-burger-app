7402f4c8497f7c6eda2f19a8ea0cc9a28f73cc26
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-05-08 21:05:39 +0100

    :construction: working on sideDrawer component

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.scss'
 
 const sideDrawer = () => {
-    return();
+    return(
+        <div className={classes.SideDrawer}>
+            <Logo />
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
+    );
 };
+
+export default sideDrawer;
