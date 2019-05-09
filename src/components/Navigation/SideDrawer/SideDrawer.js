25ba23a4895b62aada5916dd69d3aac7ce39f25e
Krzysztof Balejko <chrisbalejko86@gmail.com>
2019-05-09 22:46:45 +0100

    :construction: SideDrawer is using Backdrop

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,22 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.scss'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Aux'
 
 const sideDrawer = () => {
     return(
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-            <Logo />
+        <Aux>
+        <Backdrop />
+            <div className={classes.SideDrawer}>
+                <div className={classes.Logo}>
+                <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
